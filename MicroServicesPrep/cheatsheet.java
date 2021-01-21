9a21556bdcfc8b721b224340c1bb42337e6884da
Matthew Thomas <71943868+MatthewLThomas@users.noreply.github.com>
2021-01-21 14:42:00 -0600

    Update cheatsheet.java

--- MicroServicesPrep/cheatsheet.java
+++ MicroServicesPrep/cheatsheet.java
@@ -1,6 +1,5 @@
 //start.spring.io
 //spring web
-//spring data jpa
 //lombok
 //Eureka client / server
 
@@ -14,13 +13,8 @@
 @Data
 @Entity
 @ToString
+@Builder
 public class model {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    private field;
 
 }
 
@@ -62,7 +56,84 @@ private ApiInfo apiDetails(){
 // Eureka Server
 // use 1.8
 // app.props
-server.port = 8081
+server.port = 8761
 eureka.client.register-with-eureka=false   
 eureka.client.fetch-registry=false
+
+//Eureka Client
+spring.application.name = "NAME"
+server.port = 8081
+
+
+@EnableEurekaClient
+public class app{
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+}
+
+//restTemplate
+
+object = restTemplate.getForObject("http://micro-service-name/object-path/", Object.class);
+
+//ITERATING THROUGH LIST AND REPLACING EACH OBJECT IN LIST
+
+objects.stream().map(object -> {
+	//BLOCK OF CODE TO EXECUTE PER OBJECT
+})
+
+//------REACT-------//
+//APP.css
+@import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+
+//axios
+npm i axios
+
+axios.post(`url`,object)
+	.then(res => {
+	console.log(res);
+	console.log(res.data);
+	})
+
+axios.get(`url`)
+	.then(res =>{
+		const object = res.data;
+		thisState({... objectList, object}
+	}, [Dependency])
+
+
+
+//UseState
+const [obj, setObj] = UseState(defaultValue)
+//UseEffect
+useEffect(/*sideEffect*/()=>{
+	function()
+	return /*whenKilled*/ function()
+}
+//UseContext
+const AppContext = createContext(context)
+const AppContext = useContext(context)
+//UseLocation
+const location = useLocation();
+
+{location.pathname === "/pathname" && /*show*/ (<JSX>)}
+
+//REACT-ROUTER-DOM
+npm i reactRouterDom
+
+<Router>
+	<Route path = "/" exact render = {(props) =>{
+		<>
+			JSX TO RENDER
+		</>
+		}
+	}}
+	<Route path="/path" component = {Component}
+</Router>
+
+//instead of <a>, use <Link>
+import {Link} from 'react-router-dom'
+<Link to ="/path">TEXT</Link>
 
