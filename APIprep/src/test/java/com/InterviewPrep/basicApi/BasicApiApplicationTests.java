71083268b943c8b6c97a503e464ee7892d80e434
Matthew Thomas <71943868+MatthewLThomas@users.noreply.github.com>
2021-03-07 18:44:48 -0600

    cb prep2

---
+++ APIprep/src/test/java/com/InterviewPrep/basicApi/BasicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InterviewPrep.basicApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
